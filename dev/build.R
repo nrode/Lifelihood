3af1841e04410da2ef331433b59dac6bcbdf84db
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-11-03 08:25:22 +0100

    add CI

--- dev/build.R
+++ dev/build.R
@@ -1,10 +1,7 @@
-rm(list=ls()) # clean the entire environment
-
 devtools::load_all(compile = FALSE) # load the package (to use for development mode)
 
 devtools::test() # run tests
 
-# Documentation site
 devtools::document() # create all .Rd files using source files
 Sys.setenv(RSTUDIO_PANDOC = "/opt/homebrew/bin/") # explicit pandoc location to avoid error
 pkgdown::build_site() # build documentation site
