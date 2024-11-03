ecdd2ab343a04dc7ed9854cb2973dee7993afb0e
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-11-03 08:47:09 +0100

    add CI

--- dev/build.R
+++ dev/build.R
@@ -6,5 +6,5 @@ devtools::document() # create all .Rd files using source files
 Sys.setenv(RSTUDIO_PANDOC = "/opt/homebrew/bin/") # explicit pandoc location to avoid error
 pkgdown::build_site() # build documentation site
 
-devtools::build(path = ".") # create bundled file of the pkg
+devtools::build(path = ".") # create bundled file of the pkg (compressed in a single file)
 devtools::install() # install local development of the pkg (to allow `library(lifelihood)`)
