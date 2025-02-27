e4833bd963d44ce8f57fc26a64d83788b653d1d9
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-02-27 10:34:30 +0100

    replace mor/death start/end

--- dev/testing_things_out.R
+++ dev/testing_things_out.R
@@ -1,16 +1,27 @@
 rm(list = ls())
 devtools::load_all(compile = FALSE)
-df <- read.csv(here::here("data/raw_data/DataPierrick/100%mort_Pierrick211genoparinteraction.csv"))
+df <- read.csv(here::here(
+  "data/raw_data/DataPierrick/100%mort_Pierrick211genoparinteraction.csv"
+))
 df$geno <- as.factor(df$geno)
 df$par <- as.factor(df$par)
 df$spore <- as.factor(df$par)
 
 clutchs <- c(
-  "clutch_start1", "clutch_end1", "clutch_size1",
-  "clutch_start2", "clutch_end2", "clutch_size2"
+  "clutch_start1",
+  "clutch_end1",
+  "clutch_size1",
+  "clutch_start2",
+  "clutch_end2",
+  "clutch_size2"
 )
 generate_clutch_vector <- function(N) {
-  return(paste("pon", rep(c("start", "end", "size"), N), rep(1:N, each = 3), sep = "_"))
+  return(paste(
+    "pon",
+    rep(c("start", "end", "size"), N),
+    rep(1:N, each = 3),
+    sep = "_"
+  ))
 }
 clutchs <- generate_clutch_vector(28)
 
@@ -22,8 +33,8 @@ dataLFH <- lifelihoodData(
   maturity_start = "mat_start",
   maturity_end = "mat_end",
   clutchs = clutchs,
-  death_start = "mor_start",
-  death_end = "mor_end",
+  death_start = "death_start",
+  death_end = "death_end",
   covariates = c("par", "spore"),
   model_specs = c("wei", "lgn", "wei")
 )
@@ -44,7 +55,7 @@ results$effects
 # fonction lifelihood()
 mat <- matrix(c(as.numeric(df$par), as.numeric(df$spore)), ncol = 2)
 mat <- model.matrix(~ par + spore, data = df)
-z <- .Call(stats:::C_Cdqrls, mat, df$mor_start, 10e-3, FALSE)
+z <- .Call(stats:::C_Cdqrls, mat, df$death_start, 10e-3, FALSE)
 z$rank < ncol(mat)
 colnames(mat)[z$coefficients == 0] # raise error: model is not identifiable. check effect1, effect2 are redundants with other effects in the model!!
 data(iris)
@@ -62,9 +73,27 @@ newdata$spore <- factor(newdata$spore)
 predict(results, "expt_death", newdata)
 predict(results, "expt_death", newdata, type = "response")
 
-plot_mortality_rate(dataLFH, interval_width = 15, max_time = 170, bygroup = FALSE, log_y = TRUE)
-plot_mortality_rate(dataLFH, interval_width = 25, max_time = 170, bygroup = TRUE, log_y = TRUE)
-plot_mortality_rate(lifelihoodResults = results, interval_width = 25, max_time = 170, log_y = TRUE, prediction = TRUE)
+plot_mortality_rate(
+  dataLFH,
+  interval_width = 15,
+  max_time = 170,
+  bygroup = FALSE,
+  log_y = TRUE
+)
+plot_mortality_rate(
+  dataLFH,
+  interval_width = 25,
+  max_time = 170,
+  bygroup = TRUE,
+  log_y = TRUE
+)
+plot_mortality_rate(
+  lifelihoodResults = results,
+  interval_width = 25,
+  max_time = 170,
+  log_y = TRUE,
+  prediction = TRUE
+)
 
 mortality_rate(dataLFH, interval_width = 10)
 mortality_rate(dataLFH, interval_width = 10, bygroup = FALSE, max_time = 170)
@@ -73,64 +102,27 @@ devtools::load_all(compile = FALSE)
 pred_mortality_rate(results, interval_width = 10)
 
 
-
-
-
-
-
-
-
-
-
-
-
 df <- read.csv(here::here("data/fake_re_sample.csv"))
 df$type <- as.factor(df$type)
 df$geno <- as.factor(df$geno)
 df$event <- rep(1, nrow(df))
 df$geno_type <- as.factor(paste(df$geno, df$type, sep = "_"))
-m <- survival::survreg(Surv(
-  time = mor_start,
-  time2 = mor_end,
-  event = event,
-  type = "interval"
-) ~ geno * type, dist = "weibull", data = df)
+m <- survival::survreg(
+  Surv(
+    time = death_start,
+    time2 = death_end,
+    event = event,
+    type = "interval"
+  ) ~
+    geno * type,
+  dist = "weibull",
+  data = df
+)
 predict(m)
 
 summary(m)
 exp(m$coefficients)
 survival::predict(m)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 df <- read.csv(here::here("data/fake_sample.csv"))
@@ -139,8 +131,12 @@ df$geno <- as.factor(df$geno)
 head(df)
 
 clutchs <- c(
-  "clutch_start1", "clutch_end1", "clutch_size1",
-  "clutch_start2", "clutch_end2", "clutch_size2"
+  "clutch_start1",
+  "clutch_end1",
+  "clutch_size1",
+  "clutch_start2",
+  "clutch_end2",
+  "clutch_size2"
 )
 
 dataLFH <- lifelihoodData(
@@ -151,8 +147,8 @@ dataLFH <- lifelihoodData(
   maturity_start = "mat_start",
   maturity_end = "mat_end",
   clutchs = clutchs,
-  death_start = "mor_start",
-  death_end = "mor_end",
+  death_start = "death_start",
+  death_end = "death_end",
   covariates = c("geno", "type"),
   model_specs = c("gam", "lgn", "wei")
 )
--- tests/testthat/test-default_bounds_df.R
+++ tests/testthat/test-default_bounds_df.R
@@ -4,8 +4,12 @@ test_that("Default bounds df.", {
   df$geno <- as.factor(df$geno)
 
   clutchs <- c(
-    "clutch_start1", "clutch_end1", "clutch_size1",
-    "clutch_start2", "clutch_end2", "clutch_size2"
+    "clutch_start1",
+    "clutch_end1",
+    "clutch_size1",
+    "clutch_start2",
+    "clutch_end2",
+    "clutch_size2"
   )
   dataLFH <- lifelihoodData(
     df = df,
@@ -15,8 +19,8 @@ test_that("Default bounds df.", {
     maturity_start = "mat_start",
     maturity_end = "mat_end",
     clutchs = clutchs,
-    death_start = "mor_start",
-    death_end = "mor_end",
+    death_start = "death_start",
+    death_end = "death_end",
     covariates = c("geno", "type"),
     model_specs = c("gam", "lgn", "wei")
   )
@@ -26,21 +30,39 @@ test_that("Default bounds df.", {
   max_death <- as.numeric(bounds_df[bounds_df$param == "expt_death", "max"])
   expect_equal(max_death, 40.0)
 
-  max_maturity <- as.numeric(bounds_df[bounds_df$param == "expt_maturity", "max"])
+  max_maturity <- as.numeric(bounds_df[
+    bounds_df$param == "expt_maturity",
+    "max"
+  ])
   expect_equal(max_maturity, 8.0)
 
-  max_clutch <- as.numeric(bounds_df[bounds_df$param == "expt_reproduction", "max"])
+  max_clutch <- as.numeric(bounds_df[
+    bounds_df$param == "expt_reproduction",
+    "max"
+  ])
   expect_equal(max_clutch, 10.0)
 
-  maturity_shape_min <- as.numeric(bounds_df[bounds_df$param == "maturity_shape", "min"])
+  maturity_shape_min <- as.numeric(bounds_df[
+    bounds_df$param == "maturity_shape",
+    "min"
+  ])
   expect_equal(maturity_shape_min, 0.005)
 
-  maturity_shape_max <- as.numeric(bounds_df[bounds_df$param == "maturity_shape", "max"])
+  maturity_shape_max <- as.numeric(bounds_df[
+    bounds_df$param == "maturity_shape",
+    "max"
+  ])
   expect_equal(maturity_shape_max, 600.0)
 
-  death_shape_min <- as.numeric(bounds_df[bounds_df$param == "survival_shape", "min"])
+  death_shape_min <- as.numeric(bounds_df[
+    bounds_df$param == "survival_shape",
+    "min"
+  ])
   expect_equal(death_shape_min, 0.05)
 
-  death_shape_max <- as.numeric(bounds_df[bounds_df$param == "survival_shape", "max"])
+  death_shape_max <- as.numeric(bounds_df[
+    bounds_df$param == "survival_shape",
+    "max"
+  ])
   expect_equal(death_shape_max, 500.0)
 })
