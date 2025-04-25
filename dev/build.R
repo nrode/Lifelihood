1e9a0b426b793d59b309453d5d57774dd0db9866
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-04-25 10:16:50 +0200

    clean up data

--- dev/build.R
+++ dev/build.R
@@ -1,10 +1,10 @@
 devtools::load_all(compile = FALSE) # load the package (to use for development mode)
-
 devtools::test() # run tests
-
 devtools::document() # create all .Rd files using source files
 Sys.setenv(RSTUDIO_PANDOC = "/opt/homebrew/bin/") # explicit pandoc location to avoid error
 pkgdown::build_site(preview = TRUE) # build documentation site
+
+pkgdown::build_articles(quiet = FALSE)
 
 devtools::check() # ovrall package check
 devtools::build(path = ".") # create "bundled" file of the pkg (compressed in a single one)
