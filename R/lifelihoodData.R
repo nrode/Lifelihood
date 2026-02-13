7a141307814d9708f9476a95e82d534fade37568
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-02-13 14:09:49 +0100

    fix build

--- R/lifelihoodData.R
+++ R/lifelihoodData.R
@@ -60,38 +60,6 @@
 #' @return `lifelihoodData` object
 #'
 #' @export
-#'
-#' @examples
-#' library(lifelihood)
-#' library(tidyverse)
-#'
-#' df <- fakesample |>
-#'   mutate(
-#'     geno = as.factor(geno),
-#'     type = as.factor(type)
-#'   )
-#' head(df)
-#'
-#' clutchs <- c(
-#'   "clutch_start1", "clutch_end1", "clutch_size1",
-#'   "clutch_start2", "clutch_end2", "clutch_size2"
-#' )
-#'
-#' dataLFH <- as_lifelihoodData(
-#'   df = df,
-#'   sex = "sex",
-#'   sex_start = "sex_start",
-#'   sex_end = "sex_end",
-#'   maturity_start = "mat_start",
-#'   maturity_end = "mat_end",
-#'   clutchs = clutchs,
-#'   death_start = "death_start",
-#'   death_end = "death_end",
-#'   covariates = c("geno", "type"),
-#'   model_specs = c("gam", "lgn", "wei")
-#' )
-#'
-#' summary(data)
 as_lifelihoodData <- function(
   df,
   sex,
