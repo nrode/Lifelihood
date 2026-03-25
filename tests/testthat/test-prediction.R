669b28a68c9252c70a2bdca0437aee946a201886
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-03-25 16:21:59 +0100

    add tests for trade off simulation

--- tests/testthat/test-prediction.R
+++ tests/testthat/test-prediction.R
@@ -1,4 +1,4 @@
-testthat::test_that("predictions work", {
+test_that("predictions work", {
   path_config <- if (rlang::is_interactive()) {
     "tests/testthat/config.yaml"
   } else {
@@ -49,4 +49,63 @@ testthat::test_that("predictions work", {
   expect_true(class(pred) == "numeric")
   expect_true(class(pred_resp) == "numeric")
   expect_true(all(pred <= pred_resp))
+})
+
+test_that("Prediction with ratio expt death", {
+  df_female <- datadaphnia |>
+    as_tibble() |>
+    mutate(
+      par = as.factor(par),
+      geno = as.factor(geno),
+      spore = as.factor(spore)
+    )
+
+  df_male <- df_female |>
+    mutate(
+      sex = 1,
+      across(starts_with("pon"), ~NA_real_)
+    )
+  df <- rbind(df_female, df_male)
+
+  generate_clutch_vector <- function(N) {
+    return(paste(
+      "pon",
+      rep(c("start", "end", "size"), N),
+      rep(1:N, each = 3),
+      sep = "_"
+    ))
+  }
+  clutchs <- generate_clutch_vector(28)
+
+  lifelihoodData <- as_lifelihoodData(
+    df = df,
+    sex = "sex",
+    sex_start = "sex_start",
+    sex_end = "sex_end",
+    maturity_start = "mat_start",
+    maturity_end = "mat_end",
+    clutchs = clutchs,
+    death_start = "death_start",
+    death_end = "death_end",
+    covariates = c("par", "spore"),
+    model_specs = c("wei", "gam", "lgn")
+  )
+
+  results <- lifelihood(
+    lifelihoodData = lifelihoodData,
+    path_config = use_test_config("config_pierrick"),
+    se.fit = TRUE
+  )
+
+  preds <- prediction(
+    results,
+    "ratio_expt_death",
+    type = "response",
+    se.fit = TRUE
+  )
+
+  expect_true(all(is.na(head(preds$fitted))))
+  expect_true(all(!is.na(tail(preds$fitted))))
+  expect_true(all(is.na(head(preds$se.fitted))))
+  expect_true(all(!is.na(tail(preds$se.fitted))))
 })
